0a9aabfb64a1847ac4f833590a613ba718d46124
SherifShokry <sshokry@bluecrunch.com>
2021-11-11 18:57:04 +0200

    Initial Commit

---
+++ ModernImageLoader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ModernImageLoader
+//
+//  Created by SherifShokry on 11/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ModernImageLoader/ModernImageLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernImageLoaderApp.swift
+//  ModernImageLoader
+//
+//  Created by SherifShokry on 11/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ModernImageLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
